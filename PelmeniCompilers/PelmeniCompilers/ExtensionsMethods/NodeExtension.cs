1a627641e8774d4df26f87df2d0d788fdd6dfd4a
Mierley <88342799+Mierley@users.noreply.github.com>
2023-10-28 21:03:05 +0300

    Started analyzer

---
+++ PelmeniCompilers/PelmeniCompilers/ExtensionsMethods/NodeExtension.cs
@@ -0,0 +1,19 @@
+﻿using PelmeniCompilers.Models;
+
+namespace PelmeniCompilers.ExtensionsMethods;
+
+public static class NodeExtension
+{
+    public static void CheckSemantic(this Node node)
+    {
+        if (node.Children is not null)
+        {
+            foreach (var child in node.Children)
+            {
+                child.CheckSemantic();
+            }
+        }
+            
+        
+    }
+}
---
+++ PelmeniCompilers/PelmeniCompilers/Semantic Analyzer/SemanticAnalyzer.cs
@@ -0,0 +1,21 @@
+﻿using PelmeniCompilers.Models;
+using PelmeniCompilers.Values;
+
+namespace PelmeniCompilers.Semantic_Analyzer;
+
+public class SemanticAnalyzer
+{
+    private Node _mainNode;
+    
+    public SemanticAnalyzer(Node mainNode)
+    {
+        if (mainNode.Type != NodeType.Program)
+            throw new InvalidOperationException();
+        _mainNode = mainNode;
+    }
+
+    public void Analyze()
+    {
+        
+    }
+}
