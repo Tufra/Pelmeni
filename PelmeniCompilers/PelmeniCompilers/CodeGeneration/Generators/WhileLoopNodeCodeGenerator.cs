60bfa22bf4466b0be308304286d14e51a79aea49
Ruslan Khairov <habur331@gmail.com>
2023-11-27 19:04:28 +0300

    smth

---
+++ PelmeniCompilers/PelmeniCompilers/CodeGeneration/Generators/WhileLoopNodeCodeGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection.Metadata;
+using PelmeniCompilers.ExtensionsMethods;
+using PelmeniCompilers.Models;
+using PelmeniCompilers.Values;
+
+namespace PelmeniCompilers.CodeGeneration.Generators;
+
+public class WhileLoopNodeCodeGenerator : BaseNodeCodeGenerator
+{
+    public override NodeType GeneratingCodeNodeType => NodeType.WhileLoop;
+    
+    public override void GenerateCode(Node node, CodeGeneratorContext codeGeneratorContext)
+    {
+        var expressionNode = node.Children[0];
+        var bodyNode = node.Children[1];
+        var il = codeGeneratorContext.InstructionEncoder;
+
+        var condLabel = il.DefineLabel();
+        var endLabel = il.DefineLabel();
+
+        #region condition
+        
+        il.MarkLabel(condLabel);
+        expressionNode.GenerateCode(codeGeneratorContext);
+        il.Branch(ILOpCode.Brfalse, endLabel);
+
+        #endregion
+
+        #region body
+
+        bodyNode.GenerateCode(codeGeneratorContext);
+        il.Branch(ILOpCode.Br, condLabel);
+        
+        #endregion
+        
+        il.MarkLabel(endLabel);
+    }
+}
