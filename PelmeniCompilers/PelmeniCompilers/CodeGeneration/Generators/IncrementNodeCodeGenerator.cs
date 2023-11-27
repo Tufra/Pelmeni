fee81e8a4c7cc70eb4c55bf40791a47c4ba4f184
Tufra <60471036+Tufra@users.noreply.github.com>
2023-11-27 23:29:17 +0300

    increment decrement

---
+++ PelmeniCompilers/PelmeniCompilers/CodeGeneration/Generators/IncrementNodeCodeGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using PelmeniCompilers.ExtensionsMethods;
+using PelmeniCompilers.Models;
+using PelmeniCompilers.Values;
+
+namespace PelmeniCompilers.CodeGeneration.Generators;
+
+public class IncrementNodeCodeGenerator : BaseNodeCodeGenerator
+{
+    public override NodeType GeneratingCodeNodeType => NodeType.Increment;
+    
+    public override void GenerateCode(Node node, CodeGeneratorContext codeGeneratorContext)
+    {
+        var il = codeGeneratorContext.InstructionEncoder;
+
+        var leftValue = node.Children[0]!;
+
+        codeGeneratorContext.IsLeftValue = true;
+        leftValue.GenerateCode(codeGeneratorContext);
+        codeGeneratorContext.IsLeftValue = false;
+        
+        leftValue.GenerateCode(codeGeneratorContext);
+        
+        il.LoadConstantI8(1);
+        il.OpCode(ILOpCode.Add);
+
+        var varType = codeGeneratorContext.VariableType;
+        switch (varType)
+        {
+            case VariableType.Local:
+            {
+                il.StoreLocal(codeGeneratorContext.TokenOffset);
+                break;
+            }
+            case VariableType.Argument:
+            {
+                il.StoreArgument(codeGeneratorContext.TokenOffset);
+                break;
+            }
+            case VariableType.ArrayElement:
+            {
+                il.OpCode(ILOpCode.Stelem_ref);
+                break;
+            }
+            case VariableType.Field:
+            {
+                il.OpCode(ILOpCode.Stfld);
+                il.Token(MetadataTokens.FieldDefinitionHandle(codeGeneratorContext.TokenOffset));
+                break;
+            }
+            default:
+            {
+                throw new InvalidOperationException($"invalid variable type {varType.ToString()}");
+            }
+        }
+    }
+}
--- PelmeniCompilers/PelmeniLib/StdLib.cs
+++ PelmeniCompilers/PelmeniLib/StdLib.cs
@@ -1,23 +1,30 @@
 ﻿using System;
+using System.Globalization;
 
 namespace PelmeniLib
 {
     public class StdLib
     {
+        #region IO
+
         public static void Print(string str)
         {
             Console.Write(str);
         }
-
-        public static string IntToString(long num)
+        
+        public static void PrintLine(string str)
         {
-            return num.ToString();
+            Console.WriteLine(str);
         }
-
+        
         public static string ReadLine()
         {
             return Console.ReadLine()!;
         }
+
+        #endregion
+
+        #region String
 
         public static string StringConcat(string left, string right)
         {
@@ -28,5 +35,41 @@ namespace PelmeniLib
         {
             return a.Equals(b);
         }
+
+        public static string StringReplace(string str, string oldStr, string newStr)
+        {
+            return str.Replace(oldStr, newStr);
+        }
+
+        #endregion
+
+        #region Integer
+
+        public static string IntToString(long num)
+        {
+            return num.ToString();
+        }
+
+        public static long ParseInteger(string str)
+        {
+            return long.Parse(str);
+        }
+
+        #endregion
+
+        #region Real
+
+        public static string RealToString(double num)
+        {
+            return num.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public static double ParseReal(string str)
+        {
+            return double.Parse(str.Replace(',', '.'), 
+                CultureInfo.InvariantCulture);
+        }
+
+        #endregion
     }
 }
